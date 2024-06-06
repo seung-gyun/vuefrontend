6096b77d753768b2b8f034a3522eeadf02a7dc2e
seung-gyun <83224551+seung-gyun@users.noreply.github.com>
2024-06-07 00:05:23 +0900

    first commit
    처음 git 올리는 처음 vue프로젝트

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+@import from '.css/main.css'
+
+<template>
+  <Header></Header>
+  <Home></Home>
+  <Footer></Footer>
+</template>
+
+<script>
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+
+export default{
+  name : 'App',
+  components : {
+  Header,
+  Footer,
+  Home
+   }
+}  
+
+</script>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/seung-gyun/vueproject1',
+})
